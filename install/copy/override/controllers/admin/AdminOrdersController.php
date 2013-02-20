f0a8d15344bb985a51e13a3c59a6a18555b1e292
G-June <misc.eihwaz@gmail.com>
2013-02-20 11:47:54 +0200

    Update install/copy/override/controllers/admin/AdminOrdersController.php
    Debug in postProcess method removed, code reformatted.

--- install/copy/override/controllers/admin/AdminOrdersController.php
+++ install/copy/override/controllers/admin/AdminOrdersController.php
@@ -1,94 +1,106 @@
 <?php
-
 class AdminOrdersController extends AdminOrdersControllerCore
 {
-    public function postProcess()
+	public function postProcess()
 	{
-		if (Tools::getIsset('dbg'))
-		{
-			Db::getInstance()->Execute('DELETE FROM `' . _DB_PREFIX_ . 'order_invoice` WHERE `id_order` = 16');
-		}
-		
-        if (Tools::isSubmit('submitAddPayment') && isset($order))
-		{
-			if ($this->tabAccess['edit'] === '1')
-			{
+		if (Tools::isSubmit('submitAddPayment') && isset($order)) {
+			if ($this->tabAccess['edit'] === '1') {
 				$amount = str_replace(',', '.', Tools::getValue('payment_amount'));
 				$currency = new Currency(Tools::getValue('payment_currency'));
 				$order_has_invoice = $order->hasInvoice();
-				if ($order_has_invoice)
+				
+				if ($order_has_invoice) {
 					$order_invoice = new OrderInvoice(Tools::getValue('payment_invoice'));
-				else
+				}
+				else {
 					$order_invoice = null;
+				}
 
-				if (!Validate::isLoadedObject($order))
+				if ( ! Validate::isLoadedObject($order)) {
 					$this->errors[] = Tools::displayError('Order can\'t be found');
-				elseif (!Validate::isNegativePrice($amount))
+				}
+				elseif ( ! Validate::isNegativePrice($amount)) {
 					$this->errors[] = Tools::displayError('Amount is invalid');
-				elseif (!Validate::isString(Tools::getValue('payment_method')))
+				}
+				elseif ( ! Validate::isString(Tools::getValue('payment_method'))) {
 					$this->errors[] = Tools::displayError('Payment method is invalid');
-				elseif (!Validate::isString(Tools::getValue('payment_transaction_id')))
+				}
+				elseif ( ! Validate::isString(Tools::getValue('payment_transaction_id'))) {
 					$this->errors[] = Tools::displayError('Transaction ID is invalid');
-				elseif (!Validate::isLoadedObject($currency))
+				}
+				elseif ( ! Validate::isLoadedObject($currency)) {
 					$this->errors[] = Tools::displayError('Currency is invalid');
-				elseif ($order_has_invoice && !Validate::isLoadedObject($order_invoice))
+				}
+				elseif ($order_has_invoice && ! Validate::isLoadedObject($order_invoice)) {
 					$this->errors[] = Tools::displayError('Invoice is invalid');
-				elseif (!Validate::isDate(Tools::getValue('payment_date')))
+				}
+				elseif ( ! Validate::isDate(Tools::getValue('payment_date'))) {
 					$this->errors[] = Tools::displayError('Date is invalid');
+				}
 				else
 				{
-					if (!$order->addOrderPayment($amount, Tools::getValue('payment_method'), Tools::getValue('payment_transaction_id'), $currency, Tools::getValue('payment_date'), $order_invoice))
+					if ( ! $order->addOrderPayment($amount, Tools::getValue('payment_method'), Tools::getValue('payment_transaction_id'), $currency, Tools::getValue('payment_date'), $order_invoice)) {
 						$this->errors[] = Tools::displayError('An error occurred on adding order payment');
-					else
+					}
+					else {
 						Tools::redirectAdmin(self::$currentIndex.'&id_order='.$order->id.'&vieworder&conf=4&token='.$this->token);
+					}
 				}
 			}
-			else
+			else {
 				$this->errors[] = Tools::displayError('You do not have permission to edit here.');
+			}
                 
-            AdminController::postProcess();
+			AdminController::postProcess();
 		}
-        else
-        {
-            return parent::postProcess();
-        }
+		else {
+			return parent::postProcess();
+		}
 	}
     
 	public function renderView()
 	{
 		$order = new Order(Tools::getValue('id_order'));
-		if (!Validate::isLoadedObject($order))
+		
+		if ( ! Validate::isLoadedObject($order)) {
 			throw new PrestaShopException('object can\'t be loaded');
+		}
 
 		$customer = new Customer($order->id_customer);
-		$carrier = new Carrier($order->id_carrier);
+		$carrier  = new Carrier($order->id_carrier);
 		$products = $this->getProducts($order);
 
 		// Carrier module call
 		$carrier_module_call = null;
-		if ($carrier->is_module)
-		{
+		
+		if ($carrier->is_module) {
 			$module = Module::getInstanceByName($carrier->external_module_name);
-			if (method_exists($module, 'displayInfoByCart'))
+			
+			if (method_exists($module, 'displayInfoByCart')) {
 				$carrier_module_call = call_user_func(array($module, 'displayInfoByCart'), $order->id_cart);
+			}
 		}
 
 		// Retrieve addresses information
 		$addressInvoice = new Address($order->id_address_invoice, $this->context->language->id);
-		if (Validate::isLoadedObject($addressInvoice) && $addressInvoice->id_state)
+		
+		if (Validate::isLoadedObject($addressInvoice) && $addressInvoice->id_state) {
 			$invoiceState = new State((int)$addressInvoice->id_state);
-
-		if ($order->id_address_invoice == $order->id_address_delivery)
-		{
-			$addressDelivery = $addressInvoice;
-			if (isset($invoiceState))
-				$deliveryState = $invoiceState;
 		}
-		else
-		{
+
+		if ($order->id_address_invoice == $order->id_address_delivery) {
+			$addressDelivery = $addressInvoice;
+			
+			if (isset($invoiceState)) {
+				$deliveryState = $invoiceState;
+			}
+		}
+		else {
 			$addressDelivery = new Address($order->id_address_delivery, $this->context->language->id);
-			if (Validate::isLoadedObject($addressDelivery) && $addressDelivery->id_state)
+			
+			if (Validate::isLoadedObject($addressDelivery) && $addressDelivery->id_state) {
 				$deliveryState = new State((int)($addressDelivery->id_state));
+			}
 		}
 
 		$this->toolbar_title = sprintf($this->l('Order #%1$d (%2$s) - %3$s %4$s'), $order->id, $order->reference, $customer->firstname, $customer->lastname);
@@ -97,39 +109,41 @@ class AdminOrdersController extends AdminOrdersControllerCore
 		$warehouse_list = null;
 
 		$order_details = $order->getOrderDetailList();
-		foreach ($order_details as $order_detail)
-		{
+		
+		foreach ($order_details as $order_detail) {
 			$product = new Product($order_detail['product_id']);
 
-			if (Configuration::get('PS_ADVANCED_STOCK_MANAGEMENT')
-				&& $product->advanced_stock_management)
-			{
+			if (Configuration::get('PS_ADVANCED_STOCK_MANAGEMENT') && $product->advanced_stock_management) {
 				$warehouses = Warehouse::getWarehousesByProductId($order_detail['product_id'], $order_detail['product_attribute_id']);
-				foreach ($warehouses as $warehouse)
-				{
-					if (!isset($warehouse_list[$warehouse['id_warehouse']]))
+				
+				foreach ($warehouses as $warehouse) {
+					if ( ! isset($warehouse_list[$warehouse['id_warehouse']])) {
 						$warehouse_list[$warehouse['id_warehouse']] = $warehouse;
+					}
 				}
 			}
 		}
 
 		$payment_methods = array();
-		foreach (PaymentModule::getInstalledPaymentModules() as $payment)
-		{
+		
+		foreach (PaymentModule::getInstalledPaymentModules() as $payment) {
 			$module = Module::getInstanceByName($payment['name']);
-			if (Validate::isLoadedObject($module))
+			
+			if (Validate::isLoadedObject($module)) {
 				$payment_methods[$module->id] = $module->displayName;
+			}
 		}
 
 		// display warning if there are products out of stock
 		$display_out_of_stock_warning = false;
 		$current_order_state = $order->getCurrentOrderState();
-		if ($current_order_state->delivery != 1 && $current_order_state->shipped != 1)
+		
+		if ($current_order_state->delivery != 1 && $current_order_state->shipped != 1) {
 			$display_out_of_stock_warning = true;
+		}
 
 		// products current stock (from stock_available)
-		foreach ($products as &$product)
-		{
+		foreach ($products as &$product) {
 			$product['current_stock'] = StockAvailable::getQuantityAvailableByProduct($product['product_id'], $product['product_attribute_id'], $product['id_shop']);
 			
 			$resume = OrderSlip::getProductSlipResume((int)$product['id_order_detail']);
@@ -140,56 +154,56 @@ class AdminOrdersController extends AdminOrdersControllerCore
 			$product['return_history'] = OrderReturn::getProductReturnDetail($product['id_order_detail']);
 			
 			// if the current stock requires a warning
-			if ($product['current_stock'] == 0 && $display_out_of_stock_warning)
+			if ($product['current_stock'] == 0 && $display_out_of_stock_warning) {
 				$this->displayWarning($this->l('This product is out of stock: ').' '.$product['product_name']);
+			}
 		}
 
 		// Smarty assign
 		$this->tpl_view_vars = array(
-			'order' => $order,
-			'cart' => new Cart($order->id_cart),
-			'customer' => $customer,
-			'customer_addresses' => $customer->getAddresses($this->context->language->id),
-			'addresses' => array(
-				'delivery' => $addressDelivery,
+			'order'                        => $order,
+			'cart'                         => new Cart($order->id_cart),
+			'customer'                     => $customer,
+			'customer_addresses'           => $customer->getAddresses($this->context->language->id),
+			'addresses'                    => array(
+				'delivery'      => $addressDelivery,
 				'deliveryState' => isset($deliveryState) ? $deliveryState : null,
-				'invoice' => $addressInvoice,
-				'invoiceState' => isset($invoiceState) ? $invoiceState : null
+				'invoice'       => $addressInvoice,
+				'invoiceState'  => isset($invoiceState) ? $invoiceState : null
 			),
-			'customerStats' => $customer->getStats(),
-			'products' => $products,
-			'discounts' => $order->getCartRules(),
-			'orders_total_paid_tax_incl' => $order->getOrdersTotalPaid(), // Get the sum of total_paid_tax_incl of the order with similar reference
-			'total_paid' => $order->getTotalPaid(),
-			'returns' => OrderReturn::getOrdersReturn($order->id_customer, $order->id),
-			'customer_thread_message' => CustomerThread::getCustomerMessages($order->id_customer, 0),
-			'orderMessages' => OrderMessage::getOrderMessages($order->id_lang),
-			'messages' => Message::getMessagesByOrderId($order->id, true),
-			'carrier' => new Carrier($order->id_carrier),
-			'history' => $order->getHistory($this->context->language->id),
-			'states' => OrderState::getOrderStates($this->context->language->id),
-			'warehouse_list' => $warehouse_list,
-			'sources' => ConnectionsSource::getOrderSources($order->id),
-			'currentState' => $order->getCurrentOrderState(),
-			'currency' => new Currency($order->id_currency),
-			'currencies' => Currency::getCurrencies(),
-			'previousOrder' => $order->getPreviousOrderId(),
-			'nextOrder' => $order->getNextOrderId(),
-			'current_index' => self::$currentIndex,
-			'carrierModuleCall' => $carrier_module_call,
-			'iso_code_lang' => $this->context->language->iso_code,
-			'id_lang' => $this->context->language->id,
-			'can_edit' => ($this->tabAccess['edit'] == 1),
-			'current_id_lang' => $this->context->language->id,
-			'invoices_collection' => $order->getInvoicesCollection(),
+			'customerStats'                => $customer->getStats(),
+			'products'                     => $products,
+			'discounts'                    => $order->getCartRules(),
+			'orders_total_paid_tax_incl'   => $order->getOrdersTotalPaid(), // Get the sum of total_paid_tax_incl of the order with similar reference
+			'total_paid'                   => $order->getTotalPaid(),
+			'returns'                      => OrderReturn::getOrdersReturn($order->id_customer, $order->id),
+			'customer_thread_message'      => CustomerThread::getCustomerMessages($order->id_customer, 0),
+			'orderMessages'                => OrderMessage::getOrderMessages($order->id_lang),
+			'messages'                     => Message::getMessagesByOrderId($order->id, true),
+			'carrier'                      => new Carrier($order->id_carrier),
+			'history'                      => $order->getHistory($this->context->language->id),
+			'states'                       => OrderState::getOrderStates($this->context->language->id),
+			'warehouse_list'               => $warehouse_list,
+			'sources'                      => ConnectionsSource::getOrderSources($order->id),
+			'currentState'                 => $order->getCurrentOrderState(),
+			'currency'                     => new Currency($order->id_currency),
+			'currencies'                   => Currency::getCurrencies(),
+			'previousOrder'                => $order->getPreviousOrderId(),
+			'nextOrder'                    => $order->getNextOrderId(),
+			'current_index'                => self::$currentIndex,
+			'carrierModuleCall'            => $carrier_module_call,
+			'iso_code_lang'                => $this->context->language->iso_code,
+			'id_lang'                      => $this->context->language->id,
+			'can_edit'                     => ($this->tabAccess['edit'] == 1),
+			'current_id_lang'              => $this->context->language->id,
+			'invoices_collection'          => $order->getInvoicesCollection(),
 			'not_paid_invoices_collection' => $order->getNotPaidInvoicesCollection(),
-			'payment_methods' => $payment_methods,
-			'invoice_management_active' => Configuration::get('PS_INVOICE'),
-			'order_payments' => $order->getOrderPaymentCollection(),
-			'total_payment_fees' => $order->getTotalPaymentFees()
+			'payment_methods'              => $payment_methods,
+			'invoice_management_active'    => Configuration::get('PS_INVOICE'),
+			'order_payments'               => $order->getOrderPaymentCollection(),
+			'total_payment_fees'           => $order->getTotalPaymentFees()
 		);
 
 		return AdminController::renderView();
 	}
 }
-
