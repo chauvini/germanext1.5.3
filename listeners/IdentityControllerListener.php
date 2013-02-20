8aa88de0136b08018996cdcac675d9fe7dd25c45
G-June <misc.eihwaz@gmail.com>
2013-02-20 15:34:03 +0200

    Update listeners/IdentityControllerListener.php

--- listeners/IdentityControllerListener.php
+++ listeners/IdentityControllerListener.php
@@ -1,18 +1,14 @@
 <?php
 class IdentityControllerListener extends Germanext
 {
-	public function __construct()
-	{
-
-	}
+	public function __construct(){}
 	
-    public function execute($context)
-    {
+	public function execute($context)
+	{
 		$context->smarty->assign(array(
 			'GN_NEWSLETTER'        => self::getGernamextNewsLetterState(),
 			'GN_FORCE_STAT_GATHER' => Configuration::get('GN_FORCE_STAT_GATHER'),
 			'PS_PSTATISTIC'        => Configuration::get('PS_PSTATISTIC')
 		));
-    }
+	}
 }
-?>
